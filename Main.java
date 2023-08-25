5ecdbcb16232a67b3c3eb27e0b4c40422d62882d
Federico M <69607469+FedericoM120@users.noreply.github.com>
2023-08-25 18:40:23 -0400

    trying to fix jackson dependency

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLEncoder;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 public class Main {
     public static void main(String[] args) {
---
+++ target/classes/target/classes/target/classes/README.md
@@ -0,0 +1,2 @@
+# Weather-Fetcher
+Java program that fetches weather data from a weather API and then posts a modified version of that data to another API
